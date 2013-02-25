aea103b0998cad14ac8e6e01ef6160436fe4b411
tom <tom@ubuntu>
2013-02-25 13:12:08 +0100

    Controllers added

---
+++ src/gui/MoveController.java
@@ -0,0 +1,11 @@
+package gui;
+
+import grid.Direction;
+
+public class MoveController {
+	
+	public void movePlayer(Direction direction) {
+		
+	}
+	
+}
---
+++ src/gui/UseItemController.java
@@ -0,0 +1,15 @@
+package gui;
+
+import java.util.List;
+
+public class UseItemController {
+	
+	public List<Integer> getInventoryList() {
+		return null;
+	}
+	
+	public void use(int id) {
+		
+	}
+	
+}
