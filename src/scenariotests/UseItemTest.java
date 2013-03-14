03c2ad683f2476a224246df64f98719a66ca5cd5
tom <tom@ubuntu>
2013-03-14 14:41:31 +0100

    Tests

--- src/scenariotests/UseItemTest.java
+++ src/scenariotests/UseItemTest.java
@@ -1,8 +1,10 @@
 package scenariotests;
 
 import static org.junit.Assert.*;
+import item.IItem;
 import java.util.List;
 import game.Game;
+import grid.Direction;
 import grid.Grid;
 import grid.GridBuilder;
 import org.junit.BeforeClass;
@@ -19,10 +21,8 @@ import controllers.UseItemController;
 /**
  * Test if the use item mechanics work correctly.
  * 
- * Tests:
- * - Cannot place light grenade on wall
- * - Place maximum one lightgrenade on a square
- * - Cannot place light grenade on start positions
+ * Tests: - Place maximum one lightgrenade on a square - Cannot place light
+ * grenade on start positions
  * 
  * @author Tom
  */
@@ -30,6 +30,7 @@ public class UseItemTest {
 	
 	private static PickUpItemController	pickUpCont;
 	private static MoveController		moveCont;
+	private static EndTurnController	endTurnCont;
 	private static Grid					grid;
 	private static PlayerDataBase		playerDB;
 	
@@ -38,7 +39,6 @@ public class UseItemTest {
 		
 		playerDB = new PlayerDataBase();
 		List<IPlayer> playerList = playerDB.createNewDB();
-		
 		
 		GridBuilder builder = new GridBuilder(playerList);
 		grid = builder.getPredefinedTestGrid(playerList);
@@ -50,23 +50,66 @@ public class UseItemTest {
 		game.setGrid(grid);
 		
 		game.start();
-
+		
 		moveCont = new MoveController(playerDB);
 		pickUpCont = new PickUpItemController(playerDB);
-	}
-	
-	@Test
-	public void testCannotPlaceLightGrenadeOnWall() {
-		
+		endTurnCont = new EndTurnController(playerDB);
 	}
 	
 	@Test
 	public void testMaximumOneLightGrenadePerSquare() {
-		
+		// Player 1 actions
+		moveCont.move(Direction.NORTH);
+		moveCont.move(Direction.EAST);
+		moveCont.move(Direction.EAST);
+		// Player 2 actions
+		moveCont.move(Direction.SOUTH);
+		endTurnCont.endTurn();
+		// Player 1 actions
+		moveCont.move(Direction.EAST);
+		moveCont.move(Direction.EAST);
+		moveCont.move(Direction.EAST);
+		// Player 2 actions
+		moveCont.move(Direction.SOUTH);
+		endTurnCont.endTurn();
+		// Player 1 actions
+		List<IItem> items1 = grid.getSquareOfPlayer(playerDB.getCurrentPlayer())
+				.getCarryableItems();
+		IItem lightGrenade1 = items1.get(0);
+		pickUpCont.pickUpItem(lightGrenade1);
+		moveCont.move(Direction.EAST);
+		List<IItem> items2 = grid.getSquareOfPlayer(playerDB.getCurrentPlayer())
+				.getCarryableItems();
+		IItem lightGrenade2 = items2.get(0);
+		pickUpCont.pickUpItem(lightGrenade2);
+		// Player 2 actions
+		moveCont.move(Direction.SOUTH);
+		endTurnCont.endTurn();
+		// Player 1 actions
+		// TODO twee keer onmiddelijk droppen? niet rond lopen
 	}
 	
 	@Test
 	public void testCannotPlaceLightGrenadeOnStartPositions() {
-		
+		// Player 1 actions
+		moveCont.move(Direction.NORTHEAST);
+		moveCont.move(Direction.NORTHEAST);
+		List<IItem> items1 = grid.getSquareOfPlayer(playerDB.getCurrentPlayer())
+				.getCarryableItems();
+		IItem lightGrenade1 = items1.get(0);
+		pickUpCont.pickUpItem(lightGrenade1);
+		// player 2 Actions
+		moveCont.move(Direction.SOUTH);
+		endTurnCont.endTurn();
+		// Player 1 actions
+		moveCont.move(Direction.NORTHWEST);
+		moveCont.move(Direction.SOUTHWEST);
+		moveCont.move(Direction.SOUTH);
+		// Player 2 actions
+		moveCont.move(Direction.SOUTH);
+		endTurnCont.endTurn();
+		// Player 1 actions:
+		moveCont.move(Direction.SOUTH);
+		// TODO try to place LG
 	}
 }
