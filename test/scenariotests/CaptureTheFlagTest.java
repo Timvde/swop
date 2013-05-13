6c044783f96a56cf78b7a280db3f67ed09392886
Jo Van Bulck <jovanbulck@gmail.com>
2013-05-13 20:59:04 +0200

    check flag returned to start and player loose --> flag dropped

--- test/scenariotests/CaptureTheFlagTest.java
+++ test/scenariotests/CaptureTheFlagTest.java
@@ -2,18 +2,20 @@ package scenariotests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
-import item.IItem;
-import item.lightgrenade.LightGrenade;
-import java.util.List;
 import game.Game;
-import grid.builder.GridBuilderDirector;
+import grid.builder.DeterministicGridBuilderDirector;
+import item.IItem;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import player.IPlayer;
 import player.Player;
 import player.PlayerState;
+import square.AbstractSquare;
 import square.Direction;
+import square.Square;
 import controllers.NewGameController;
 
 /**
@@ -52,7 +54,7 @@ public class CaptureTheFlagTest extends SetupTestGrid {
 		
 		// SetupTestGrid will create the test grid with two player in the
 		// @Before
-		newGameCont.newCTFGame(GridBuilderDirector.NUMBER_OF_PLAYERS);
+		newGameCont.newCTFGame(DeterministicGridBuilderDirector.NUMBER_OF_PLAYERS_ON_TEST_GRID);
 	}
 	
 	/**
@@ -61,6 +63,9 @@ public class CaptureTheFlagTest extends SetupTestGrid {
 	 * players, i.e. pick up the flag of another player and return it to your
 	 * own starting location. A player wins the game when he has captured the
 	 * flag of each other player at least once
+	 * 
+	 * When a player captures a flag, that flag is returned to its starting
+	 * location.
 	 */
 	@Test
 	public void testPlayerWinsWhenCapturedAllTheFlags() {
@@ -107,6 +112,67 @@ public class CaptureTheFlagTest extends SetupTestGrid {
 		// now player 1 is back on his start with the flag of player 2 --> he
 		// wins
 		assertEquals(PlayerState.FINISHED, ((Player) player1).getPlayerState());
+		
+		// When a player captures a flag, that flag is returned to its starting
+		// location.
+		AbstractSquare startPos2 = grid
+				.getSquareAt(DeterministicGridBuilderDirector.PLAYER2_START_POS);
+		List<IItem> itemlist = startPos2.getAllItems();
+		assertTrue(itemlist.contains(flag));
 	}
 	
+	/**
+	 * Tests: If a player is boxed in, such that he cannot make a move, the
+	 * player dies and is removed from the game and disappears. If he was
+	 * carrying a flag, the flag is dropped on the square where the player was
+	 * last alive.
+	 */
+	@Test
+	public void testPlayerDies() {
+		// Player 1
+		IPlayer player1 = playerDB.getCurrentPlayer();
+		moveCont.move(Direction.SOUTH);
+		moveCont.move(Direction.SOUTH);
+		// now player 1 is teleported
+		moveCont.move(Direction.SOUTH);
+		endTurnCont.endTurn();
+		
+		// player 2 (end turn asap)
+		assertNotSame(player1, playerDB.getCurrentPlayer());
+		moveCont.move(Direction.EAST);
+		endTurnCont.endTurn();
+		
+		// player 1
+		assertEquals(player1, playerDB.getCurrentPlayer());
+		moveCont.move(Direction.SOUTH);
+		// now player 1 is on the startsquare of player 2
+		assertNotSame(PlayerState.FINISHED, ((Player) player1).getPlayerState());
+		// pickup the flag:
+		List<IItem> itemsList = playerDB.getCurrentPlayer().getCurrentLocation()
+				.getCarryableItems();
+		assertEquals(1, itemsList.size());
+		assertTrue(itemsList.get(0) instanceof Flag);
+		Flag flag = (Flag) itemsList.get(0);
+		pickUpCont.pickUpItem(Flag);
+		// (now player 1 has to return the flag to his start)
+		moveCont.move(Direction.NORTH);
+		endTurnCont.endTurn();
+		
+		//player 2
+		assertNotSame(player1, playerDB.getCurrentPlayer());
+		moveCont.move(Direction.EAST);
+		endTurnCont.endTurn();
+		
+		//player 1
+		assertEquals(player1, playerDB.getCurrentPlayer());
+		Square player1LastSq = player1.getCurrentLocation();
+		// end player 1 turn without moving --> he will loose
+		endTurnCont.endTurn();
+		assertSame(PlayerState.LOST, ((Player) player1).getPlayerState());
+		
+		// check flag is dropped:
+		List<IItem> list = player1LastSq.getCarryableItems();
+		assertEquals(1, list.size());
+		assertTrue(list.contains(flag));
+	}
 }
